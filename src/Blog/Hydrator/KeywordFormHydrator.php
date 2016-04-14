93c5d91e660583640e450d71e0c99084ee7559c4
QBonaventure <quentin@bonaventure.xyz>
2016-04-14 17:02:56 +0200

    Initial commit

---
+++ src/Blog/Hydrator/KeywordFormHydrator.php
@@ -0,0 +1,29 @@
+<?php
+namespace Blog\Hydrator;
+
+use Zend\Stdlib\Hydrator\HydratorInterface;
+use Zend\Hydrator\ClassMethods;
+
+class KeywordFormHydrator extends ClassMethods implements HydratorInterface
+{
+	public function __construct($underscoreSeparatedKeys = true) {
+        parent::__construct($underscoreSeparatedKeys);
+	}
+	
+	public function extract($object) {
+		var_dump($object);
+// 		$result['word']	= $object->getWord();
+// 		$result['id']		= $object->getid();
+// 		$result['content']	= $object->getBody();
+// 		$result['category']	= $object->getCategoryId();
+// 		$result['status']	= $object->getStatusId();
+	
+// 		foreach($object->getKeywords() as $keyword) {
+// 			$result['keywords'][] = array('id' => $keyword->getId(),
+// 					'word' => $keyword->getWord());
+// 		}
+	
+		return $result;
+	}
+	
+}
