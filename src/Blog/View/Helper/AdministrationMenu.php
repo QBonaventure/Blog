08dcbdf3054d5edd7e7dc7063b4c7e9f406a4dc2
QBonaventure <quentin@bonaventure.xyz>
2016-05-16 00:51:54 +0200

    Small fix

--- src/Blog/View/Helper/AdministrationMenu.php
+++ src/Blog/View/Helper/AdministrationMenu.php
@@ -6,7 +6,7 @@ use Zend\View\Helper\AbstractHelper;
 class AdministrationMenu extends AbstractHelper {
 	
 	public function __invoke($subMenu = null) {
-		if($subMenu)
+		if($subMenu) 
 			return $this->getView()->render('blog/administration/partials/'.$subMenu.'-menu');
 		return $this->getView()->render('blog/administration/partials/menu');
 	}
