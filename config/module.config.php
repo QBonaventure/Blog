46f0b36b91dbcf8133d6481565022738bc9086e3
QBonaventure <quentin@bonaventure.xyz>
2016-05-16 14:37:12 +0200

    Fixed the post edit view

--- config/module.config.php
+++ config/module.config.php
@@ -204,14 +204,14 @@ return array(
             ),
         ),
     ),
-	'mail' => array(
-		'host'              => 'imap.bonaventure.xyz',
-		'port'				=> 587,
-		'connection_class'  => 'plain',
-		'connection_config' => array(
-				'username' => 'quentin@bonaventure.xyz',
-				'password' => 'bNMpz2kjEf8nlH71sHESXEWf2sSrQp',
-				'ssl'	=> 'tls',
-		),
-	),
+// 	'mail' => array(
+// 		'host'              => 'imap.bonaventure.xyz',
+// 		'port'				=> 587,
+// 		'connection_class'  => 'plain',
+// 		'connection_config' => array(
+// 				'username' => 'quentin@bonaventure.xyz',
+// 				'password' => 'bNMpz2kjEf8nlH71sHESXEWf2sSrQp',
+// 				'ssl'	=> 'tls',
+// 		),
+// 	),
 );
